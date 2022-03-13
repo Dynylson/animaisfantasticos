f9557ce59d32c74e577fb008f8a93b0b3a619127
Dynylson <dynylsonjr@gmail.com>
2022-03-12 23:14:22 -0300

    modal e tooltip adicionados

---
+++ 0302-html-e-css-do-projeto/js/script.js
@@ -0,0 +1,13 @@
+import initScrollSmooth from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+
+initScrollSmooth();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
