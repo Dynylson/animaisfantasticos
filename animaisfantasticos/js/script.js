d841305fc715cc287f688d2dd426e0945aabab75
Dynylson <dynylsonjr@gmail.com>
2022-05-07 16:07:35 -0300

    refatorado o accordion para classe

--- animaisfantasticos/js/script.js
+++ animaisfantasticos/js/script.js
@@ -1,6 +1,6 @@
 import { ScrollSmooth } from "./modules/scroll-suave.js";
+import { Accordion } from "./modules/accordion.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
-import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -14,8 +14,10 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 const scrollSmooth = new ScrollSmooth("[data-menu='suave'] a[href^='#']");
 scrollSmooth.init();
 
+const accordion = new Accordion("[data-anime='accordion'] dt");
+accordion.init();
+
 initAnimacaoScroll();
-initAccordion();
 initTabNav();
 initModal();
 initTooltip();
